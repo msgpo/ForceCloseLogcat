c0b8da8e756bce42bba4fd6a90a54cff0d4d4519
ryuunoakaihitomi <18475860917@163.com>
2018-07-12 11:50:11 +0800

    添加存储权限检查

--- app/src/main/java/com/ryuunoakaihitomi/ForceCloseLogcat/MainActivity.java
+++ app/src/main/java/com/ryuunoakaihitomi/ForceCloseLogcat/MainActivity.java
@@ -1,14 +1,22 @@
 package com.ryuunoakaihitomi.ForceCloseLogcat;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AppOpsManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Process;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (ConfigMgr.getBoolean(ConfigMgr.Options.FIRST_RUN)) {
+            //noinspection ConstantConditions
+            if (checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, Process.myPid(), Process.myUid()) != PackageManager.PERMISSION_GRANTED
+                    || ((AppOpsManager) getSystemService(APP_OPS_SERVICE)).checkOpNoThrow(AppOpsManager.OPSTR_WRITE_EXTERNAL_STORAGE, Process.myUid(), getPackageName()) != AppOpsManager.MODE_ALLOWED)
+                Utils.simpleToast(getApplicationContext(), getString(R.string.need_wes_perm_notice), false, true);
             startActivity(new Intent(MyApplication.getContext(), ConfigUI.class));
             startService(new Intent(this, FCLogService.class));
         } else {
